f281b9436c131f65b587ea3e256a70d7606b1ffd
Azbul <nabiev.bulat@mail.ru>
2022-07-13 16:57:45 +0300

    убрал ненужные юсинги

--- Рефакторинг/DataStructure/Inheritance.DataStructure.refacted/Category_should.cs
+++ Рефакторинг/DataStructure/Inheritance.DataStructure.refacted/Category_should.cs
@@ -1,9 +1,5 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 // ReSharper disable IsExpressionAlwaysTrue
 
 namespace Inheritance.DataStructure
